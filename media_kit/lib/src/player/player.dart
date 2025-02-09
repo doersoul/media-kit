f9fa5110de5442e5ebc903ae86a92acd53a24ca6
doer <doersoult@126.com>
2025-02-09 15:35:33 +0800

    update

--- media_kit/lib/src/player/player.dart
+++ media_kit/lib/src/player/player.dart
@@ -4,20 +4,19 @@
 /// All rights reserved.
 /// Use of this source code is governed by MIT license that can be found in the LICENSE file.
 import 'dart:typed_data';
-import 'package:universal_platform/universal_platform.dart';
 
-import 'package:media_kit/src/models/track.dart';
-import 'package:media_kit/src/models/playable.dart';
-import 'package:media_kit/src/models/playlist.dart';
-import 'package:media_kit/src/models/media/media.dart';
 import 'package:media_kit/src/models/audio_device.dart';
+import 'package:media_kit/src/models/media/media.dart';
+import 'package:media_kit/src/models/playable.dart';
 import 'package:media_kit/src/models/player_state.dart';
-import 'package:media_kit/src/models/playlist_mode.dart';
 import 'package:media_kit/src/models/player_stream.dart';
-
+import 'package:media_kit/src/models/playlist.dart';
+import 'package:media_kit/src/models/playlist_mode.dart';
+import 'package:media_kit/src/models/track.dart';
 import 'package:media_kit/src/player/native/player/player.dart';
-import 'package:media_kit/src/player/web/player/player.dart';
 import 'package:media_kit/src/player/platform_player.dart';
+import 'package:media_kit/src/player/web/player/player.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 /// {@template player}
 ///
@@ -121,6 +120,9 @@ class Player {
   /// Platform specific internal implementation initialized depending upon the current platform.
   PlatformPlayer? platform;
 
+  /// Disposes the [Player] instance & releases the resources.
+  bool disposing = false;
+
   /// Current state of the [Player].
   PlayerState get state => platform!.state;
 
@@ -133,6 +135,8 @@ class Player {
 
   /// Disposes the [Player] instance & releases the resources.
   Future<void> dispose() async {
+    disposing = true;
+
     return platform?.dispose();
   }
 
